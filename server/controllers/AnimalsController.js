ce9f72cdc5f2f8048df54bcedc193b989e3c2f72
Tunnelvision77 <Tacomablues666@gmail.com>
2020-10-01 15:27:15 -0600

    service and controller methods completed

--- server/controllers/AnimalsController.js
+++ server/controllers/AnimalsController.js
@@ -7,12 +7,14 @@ export class AnimalsController extends BaseController {
   constructor() {
     super("api/animals");
     this.router
-      .get("", this.getAll)
+      .get("", this.getAllAnimals)
       .post("", this.createMany)
       // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
-      .use(auth0Provider.getAuthorizedUserInfo);
+      .use(auth0Provider.getAuthorizedUserInfo)
+      .put("/:id", this.editAnimal)
+      .delete("/:id", this.deleteAnimal);
   }
-  async getAll(req, res, next) {
+  async getAllAnimals(req, res, next) {
     try {
       let data = await animalsService.findAll(req.query);
       return res.send(data);
@@ -20,10 +22,35 @@ export class AnimalsController extends BaseController {
       next(error);
     }
   }
+  async editAnimal(req, res, next) {
+    try {
+      req.body.creatorEmail = req.userInfo.email.toLowerCase();
+      await animalsService.edit(req.params.id, req.body.creatorEmail, req.body);
+      return res.send("successfully Deleted");
+    } catch (error) {
+      next(error);
+    }
+  }
   async createMany(req, res, next) {
     try {
-      let data = await animalsService.createMany(req.body);
+      if (req.body == []) {
+        let data = await animalsService.createMany(req.body);
+        return res.status(201).send(data);
+      }
+      let data = await animalsService.create(req.body);
       return res.status(201).send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteAnimal(req, res, next) {
+    try {
+      req.body.creatorEmail = req.userInfo.email.toLowerCase();
+      let data = await animalsService.deleteById(
+        req.params.id,
+        req.body.creatorEmail
+      );
     } catch (error) {
       next(error);
     }
--- server/services/AnimalsService.js
+++ server/services/AnimalsService.js
@@ -2,10 +2,6 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class AnimalsService {
-  async createMany(array) {
-    let data = await dbContext.Animals.insertMany(array);
-    return data;
-  }
   async findAll(query = {}) {
     let values = await dbContext.Animals.find(query).populate(
       "creator",
@@ -19,6 +15,34 @@ class AnimalsService {
       throw new BadRequest("Invalid Id");
     }
     return value;
+  }
+  async createMany(array) {
+    let data = await dbContext.Animals.insertMany(array);
+    return data;
+  }
+  async create(rawData) {
+    let data = await dbContext.Animals.create(rawData);
+    return data;
+  }
+  async edit(id, creatorEmail, update) {
+    let data = await dbContext.Animals.findOneAndUpdate(
+      { _id: id, creatorEmail: creatorEmail },
+      update,
+      { new: true }
+    );
+    if (!data) {
+      throw new BadRequest("Invalid ID or you do not have permissions");
+    }
+    return data;
+  }
+  async deleteById(id, creatorEmail) {
+    let data = await dbContext.Animals.findByIdAndDelete({
+      _id: id,
+      creatorEmail: creatorEmail,
+    });
+    if (!data) {
+      throw new BadRequest("Invalid Id or you are not Authorized");
+    }
   }
 }
 
