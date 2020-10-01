5085107ed984e2662be1536f3d7de63e3836e9d4
Tunnelvision77 <Tacomablues666@gmail.com>
2020-10-01 16:12:56 -0600

    we now have a data base

--- server/models/Animal.js
+++ server/models/Animal.js
@@ -1,11 +1,56 @@
 import mongoose from "mongoose";
-const Schema = mongoose.Schema;
+import { dbContext } from "../db/DbContext";
+let Schema = mongoose.Schema;
+
+const breedSchema = new Schema({
+  primary: { type: String, default: null },
+  secondary: { type: String, default: null },
+  mixed: { type: Boolean, default: false },
+  unknown: { type: Boolean, default: false },
+});
+
+const colorSchema = new Schema({
+  primary: { type: String, default: null },
+  secondary: { type: String, default: null },
+  tertiary: { type: String, default: null },
+});
+
+const attributeSchema = new Schema({
+  spayed_neutered: { type: Boolean, required: true, default: false },
+  house_trained: { type: Boolean, required: true, default: false },
+  declawed: { type: Boolean, default: false },
+  special_needs: { type: Boolean, required: true, default: false },
+  shots_current: { type: Boolean, required: true, default: false },
+});
+
+const fullSchema = new Schema({
+  full: { type: String, default: null },
+});
+
+const contactSchema = new Schema({
+  email: { type: String, required: true, default: null },
+  phone: { type: String, required: true, default: null },
+});
+const environmentSchema = new Schema({
+  children: { type: Boolean, default: false },
+});
 
 const Animal = new Schema(
   {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    creatorEmail: { type: String, required: true },
+    organization_id: { type: String, required: true, default: null },
+    species: { type: String, required: true, default: null },
+    breeds: breedSchema,
+    colors: colorSchema,
+    age: { type: String, default: null },
+    gender: { type: String, required: true, default: null },
+    size: { type: String, required: true, default: null },
+    attributes: attributeSchema,
+    environment: environmentSchema,
+    name: { type: String, required: true, default: null },
+    description: { type: String, default: null },
+    photos: [fullSchema],
+    status: { type: String, required: true },
+    contact: contactSchema,
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
@@ -16,5 +61,4 @@ Animal.virtual("creator", {
   foreignField: "email",
   justOne: true,
 });
-
 export default Animal;
