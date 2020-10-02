bed0a5ae2fa67b5c7daa27f09109d0bc918559a4
Joe Almanza <joealmanza2113@gmail.com>
2020-10-02 14:32:23 -0600

    starting swipe page

---
+++ clientsrc/src/Pages/Swipe.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="swipe container-fluid">
+    <div class="row justify-content-around">
+      <div class="col-2">
+        <i class="fa fa-minus" aria-hidden="true"></i>
+      </div>
+      <swipe-card-comp
+        v-for="animal in animals"
+        :key="animal.id"
+        :animalProp="animal"
+      />
+      <div class="col-2">
+        <i class="fa fa-plus" aria-hidden="true"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SwipeCardComp from "../components/SwipeCardComp.vue";
+export default {
+  name: "component",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.$store.dispatch("getResource", {
+      path: "animals",
+      resource: "animals",
+    });
+  },
+  computed: {
+    animals() {
+      return this.$store.state.animals;
+    },
+  },
+  methods: {},
+  components: {
+    SwipeCardComp,
+  },
+};
+</script>
+
+<style scoped></style>
