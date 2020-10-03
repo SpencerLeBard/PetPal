53f26fadbd4a3aa10cb08cd28a3b59a4ce456645
SpencerLeBard <slebard@gmail.com>
2020-10-03 15:52:07 -0600

    started to edit

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+const searchSchema = new Schema({
+  city: { type: String, default: null },
+  state: { type: String, default: null },
+  zip: { type: String, default: null },
+  dog: { type: Boolean, default: false },
+  cat: { type: Boolean, default: false },
+});
+
 const Profile = new Schema(
   {
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
     picture: { type: String },
-    // NOTE If you wish to add additional public properties for profiles do so here
+    search: searchSchema,
+    hasOrg: { type: Boolean, default: false },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
