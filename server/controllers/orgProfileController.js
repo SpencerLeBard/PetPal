623122656db839cbe06d6fac257c4c64cfe10e86
JustinBarnard1 <jbarnard0031@gmail.com>
2020-10-03 15:50:04 -0600

    server done untested

--- server/controllers/orgProfileController.js
+++ server/controllers/orgProfileController.js
@@ -2,6 +2,7 @@ import express from "express";
 import BaseController from "../utils/BaseController";
 import { orgProService } from "../services/OrgProfileService";
 import auth0Provider from "@bcwdev/auth0provider"
+import { animalsService } from "../services/AnimalsService"
 
 export class OrgProfileController extends BaseController {
   constructor() {
@@ -48,6 +49,26 @@ export class OrgProfileController extends BaseController {
       req.body.creatorEmail = req.userInfo.email.toLowerCase();
       let data = await orgProService.edit(req.params.id, req.body.creatorEmail, req.body)
       res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editAnimal(req, res, next) {
+    try {
+      req.body.creatorEmail = req.userInfo.email.toLowerCase();
+      let data = await animalsService.edit(req.params.id, req.body.creatorEmail, req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteAnimal(req, res, next) {
+    try {
+      req.body.creatorEmail = req.userInfo.email.toLowerCase();
+      await animalsService.deleteById(req.params.id, req.body.creatorEmail)
+      return res.send("Successfully Deleted!")
     } catch (error) {
       next(error)
     }
