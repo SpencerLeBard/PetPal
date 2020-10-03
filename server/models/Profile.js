73ec4e02e744a34540810f98096f1bedbfd90b3d
SpencerLeBard <slebard@gmail.com>
2020-10-03 15:53:04 -0600

    edit

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const searchSchema = new Schema({
-  city: { type: String, default: null },
-  state: { type: String, default: null },
-  zip: { type: String, default: null },
-  dog: { type: Boolean, default: false },
-  cat: { type: Boolean, default: false },
-});
+// const searchSchema = new Schema({
+//   city: { type: String, default: null },
+//   state: { type: String, default: null },
+//   zip: { type: String, default: null },
+//   dog: { type: Boolean, default: false },
+//   cat: { type: Boolean, default: false },
+// });
 
 const Profile = new Schema(
   {
@@ -15,7 +15,7 @@ const Profile = new Schema(
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
     picture: { type: String },
-    search: searchSchema,
+    // search: searchSchema,
     hasOrg: { type: Boolean, default: false },
   },
   { timestamps: true, toJSON: { virtuals: true } }
