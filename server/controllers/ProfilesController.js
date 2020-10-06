33fa3b49dbc1e24179ab04ae9766ee56c547bb97
JustinBarnard1 <jbarnard0031@gmail.com>
2020-10-06 15:13:08 -0600

    favorites controller/service/schema done

--- server/controllers/ProfilesController.js
+++ server/controllers/ProfilesController.js
@@ -2,7 +2,7 @@ import express from "express";
 import BaseController from "../utils/BaseController";
 import auth0Provider from "@bcwdev/auth0provider";
 import { profilesService } from "../services/ProfilesService";
-// import { FavoritesController } from "./FavoritesController";
+import { FavoritesController } from "./FavoritesController";
 
 export class ProfilesController extends BaseController {
   constructor() {
@@ -11,7 +11,7 @@ export class ProfilesController extends BaseController {
       .use(auth0Provider.getAuthorizedUserInfo)
       .get("", this.getUserProfile)
       .put("/:id", this.edit)
-    // .use("/:id/favorites", new FavoritesController().router)
+      .use("/favorites", new FavoritesController().router)
   }
   async getUserProfile(req, res, next) {
     try {
