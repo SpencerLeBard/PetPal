5083ffafe45b4483d1a3e5c9105759ec1d58539b
JustinBarnard1 <jbarnard0031@gmail.com>
2020-10-07 10:20:22 -0600

    footer fixed

--- clientsrc/src/components/MyFooter.vue
+++ clientsrc/src/components/MyFooter.vue
@@ -1,12 +1,16 @@
 <template>
   <footer class="container-fluid bg-dark footer">
     <div class="row">
-      <div class="col-6 d-flex justify-content-center profile-link">
+      <div
+        class="profile col-6 d-flex justify-content-center profile-link align-items-center"
+      >
         <router-link class="text-light" :to="{ name: 'Profile' }">
           <i class="fa fa-user text-light fa-3x" aria-hidden="true"></i>
         </router-link>
       </div>
-      <div class="col-6 d-flex justify-content-center home-link">
+      <div
+        class="swipe col-6 d-flex justify-content-center home-link align-items-center"
+      >
         <router-link class="text-light" :to="{ name: 'Swipe' }"
           ><i class="fa fa-home text-light fa-3x" aria-hidden="true"></i>
         </router-link>
@@ -21,8 +25,14 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .footer {
+  height: 10vh;
+}
+.profile {
+  height: 10vh;
+}
+.swipe {
   height: 10vh;
 }
 </style>
