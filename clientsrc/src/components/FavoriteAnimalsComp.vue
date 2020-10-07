8db63a560ca1cb1a52071a9d53148f1e15386edc
SpencerLeBard <slebard@gmail.com>
2020-10-07 11:19:28 -0600

    can see favorited animals on profile

---
+++ clientsrc/src/components/FavoriteAnimalsComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fav-pets">
+    <!-- <router-link
+      class="card bg-primary rounded"
+      :to="{name:'favorite-animals', params:{animalId: favAnimalsProp.id}}"
+    > -->
+    <!-- <p v-if="favAnimalsProp.value">No Favorite Animals :(</p> -->
+    <p>{{ favAnimalProp.animalId.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "favorite-animals-component",
+  props: ["favAnimalProp"],
+  data() {
+    return {};
+  },
+  methods: {
+    profile() {
+      return this.$store.state.profile;
+    },
+  },
+};
+</script>
+
+<style></style>
--- clientsrc/src/components/MyFooter.vue
+++ clientsrc/src/components/MyFooter.vue
@@ -1,16 +1,14 @@
 <template>
-  <footer
-    class=" container fixed-bottom card-footer bg-dark footer-container footer"
-  >
+  <footer class="container-fluid fixed-bottom bg-dark footer">
     <div class="row">
       <div class="col-6 d-flex justify-content-center profile-link">
         <router-link class="text-light" :to="{ name: 'Profile' }">
-          <i class="fa fa-user text-light fa-5x" aria-hidden="true"></i>
+          <i class="fa fa-user text-light fa-3x m-3" aria-hidden="true"></i>
         </router-link>
       </div>
-      <div class="col-6 d-flex justify-content-center home-link">
+      <div class="col-6 d-flex justify-content-center">
         <router-link class="text-light " :to="{ name: 'Swipe' }"
-          ><i class="fa fa-home text-light fa-5x" aria-hidden="true"></i>
+          ><i class="fa fa-home text-light fa-3x m-3" aria-hidden="true"></i>
         </router-link>
       </div>
     </div>
@@ -25,6 +23,10 @@ export default {
 
 <style>
 .footer {
-  height: 14vh;
+  height: 10vh;
+  position: relative;
+  bottom: 0;
+  left: 0;
+  width: 100%;
 }
 </style>
