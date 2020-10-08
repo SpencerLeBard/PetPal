711b696d8840c3040462bd2e2352fb580426525e
MatthewHufft <matthufft777@gmail.com>
2020-10-08 14:37:21 -0600

    only styling changes from this point

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container-fluid getStarted">
     <div class="row">
-      <div v-if="!profile">
+      <div v-if="!profile.name">
         <h2 class="text-center text-light mt-5">
           Please Login/SignUp And Save Lives!
         </h2>
       </div>
       <div
-        v-if="!profile.completedQuiz"
+        v-if="profile.completedQuiz"
         class="col-12 d-flex flex-column text-center"
       >
         <div class="div" v-if="question === 0">
